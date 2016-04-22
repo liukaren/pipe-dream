44af6b9f8c08c1fd4f8937542ebb1ae0815b2331
Karen Liu <karenliu1@gmail.com>
2016-04-22 14:56:47 -0700

    Add a simple goo-fill animation.

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,5 +1,4 @@
 import React, { PropTypes as Type } from 'react'
-import cn from 'classnames'
 
 import { TileType } from 'constants'
 import { StartTiles } from 'tiles'
@@ -22,18 +21,26 @@ export default React.createClass({
 
         let tileStyle = {}
         if (tile.type.image) {
-            tileStyle.backgroundImage = `url(public/images/${tile.type.image})`
+            tileStyle.backgroundImage = `url(public/images/${tile.type.image}.png)`
+        }
+
+        let gooStyle = {}
+        if (tile.type.image) {
+            gooStyle.backgroundImage = `url(public/images/${tile.type.image}-fill.png)`
+            // TODO: Set gooStyle.animationDuration to a variable amount of time
         }
 
         let nextTileStyle = {}
         if (nextTile && nextTile.type.image) {
-            nextTileStyle.backgroundImage = `url(public/images/${nextTile.type.image})`
+            nextTileStyle.backgroundImage = `url(public/images/${nextTile.type.image}.png)`
         }
 
         // Show the current tile, with an optional ghost preview of the next tile.
         return <div onClick={ () => { isChangeableTile && this.props.onClick() } }
-                    className={ cn(styles.tile, tile.hasGoo && styles.hasGoo) }
+                    className={ styles.tile }
                     style={ tileStyle }>
+            { tile.hasGoo &&
+                <div className={ styles.tileGoo } style={ gooStyle }></div> }
             { nextTile && isChangeableTile &&
                  <div className={ styles.tilePreview }
                       style={ nextTileStyle }></div> }
--- src/tiles.js
+++ src/tiles.js
@@ -3,37 +3,37 @@ export const Tiles = {
     UP_RIGHT: {
         id: 1,
         openings: [1, 2],
-        image: 'up-right.png'
+        image: 'up-right'
     },
     UP_DOWN: {
         id: 2,
         openings: [1, 3],
-        image: 'up-down.png'
+        image: 'up-down'
     },
     UP_LEFT: {
         id: 3,
         openings: [1, 4],
-        image: 'up-left.png'
+        image: 'up-left'
     },
     RIGHT_DOWN: {
         id: 4,
         openings: [2, 3],
-        image: 'right-down.png'
+        image: 'right-down'
     },
     RIGHT_LEFT: {
         id: 5,
         openings: [2, 4],
-        image: 'right-left.png'
+        image: 'right-left'
     },
     DOWN_LEFT: {
         id: 6,
         openings: [3, 4],
-        image: 'down-left.png'
+        image: 'down-left'
     },
     CROSS: {
         id: 7,
         openings: [1, 2, 3, 4],
-        image: 'cross.png'
+        image: 'cross'
     },
     EMPTY: {
         id: 8,
@@ -44,21 +44,21 @@ export const StartTiles = {
     UP: {
         id: 1,
         openings: [1],
-        image: 'start-up.png'
+        image: 'start-up'
     },
     RIGHT: {
         id: 2,
         openings: [2],
-        image: 'start-right.png'
+        image: 'start-right'
     },
     DOWN: {
         id: 3,
         openings: [3],
-        image: 'start-down.png'
+        image: 'start-down'
     },
     LEFT: {
         id: 4,
         openings: [4],
-        image: 'start-left.png'
+        image: 'start-left'
     }
 }
