719589f15700db480b3922fe25d34e274eb4598e
Karen Liu <karenliu1@gmail.com>
2016-04-26 17:32:31 -0700

    Show a basic game over screen.

--- src/components/GameOver/index.jsx
+++ src/components/GameOver/index.jsx
@@ -5,7 +5,13 @@ import styles from './styles.less'
 export default React.createClass({
     render() {
         return <div className={ styles.main }>
-            GAME OVER
+            <div className={ styles.word1 }>
+                <img src="../../../public/images/gameover1.svg" />
+            </div>
+            <div className={ styles.space }></div>
+            <div className={ styles.word2 }>
+                <img src="../../../public/images/gameover2.svg" />
+            </div>
         </div>
     }
 })
