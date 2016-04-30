10ed6638e5f19fa4ff5d53125bf8ee23d4ce73e5
Karen Liu <karenliu1@gmail.com>
2016-04-29 21:55:11 -0700

    Add a 'rules' button to start screen.

--- src/components/GameStart/index.jsx
+++ src/components/GameStart/index.jsx
@@ -4,15 +4,24 @@ import styles from './styles.less'
 
 export default React.createClass({
     propTypes: {
-        onStartClick: React.PropTypes.func
+        onStartClick: React.PropTypes.func,
+        onRulesClick: React.PropTypes.func
     },
 
     render() {
         return <div className={ styles.main }>
             <div className={ styles.row }>
-                <img src="../../../public/images/start.svg"
-                     className={ styles.start }
-                     onClick={ this.props.onStartClick } />
+                <div className={ styles.col }>
+                    <img src="../../../public/images/start.svg"
+                         className={ styles.button }
+                         onClick={ this.props.onStartClick } />
+                </div>
+                <div className={ styles.spacer }></div>
+                <div className={ styles.col }>
+                    <img src="../../../public/images/rules.svg"
+                         className={ styles.button }
+                         onClick={ this.props.onRulesClick } />
+                </div>
             </div>
         </div>
     }
